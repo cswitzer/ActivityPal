8e74293d980ce7dcd625945e2349b4754ffb4e8b
Chase <chasehillig@yahoo.com>
2022-03-25 18:42:31 -0600

    make home UI better

--- app/src/main/java/com/example/activitypal/utils/HomeAdapter.java
+++ app/src/main/java/com/example/activitypal/utils/HomeAdapter.java
@@ -1,5 +1,7 @@
 package com.example.activitypal.utils;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,10 +11,12 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.activitypal.R;
 import com.example.activitypal.models.Activity;
 
 import java.util.ArrayList;
+import java.util.Base64;
 
 public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder> {
     ArrayList<Activity> data;
@@ -27,7 +31,20 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder> 
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        holder.activityName.setText(data.get(position).getName());
+        holder.activityElementName.setText(data.get(position).getName());
+        holder.activityElementLocation.setText(data.get(position).getAddress());
+        holder.activityStartTime.setText(data.get(position).getStartTime());
+        holder.activityEndTime.setText(data.get(position).getEndTime());
+        // holder.activityId.setText(data.get(position).get_id());
+
+        // convert base64 string to bitmap
+        byte[] decodedStringImg = Base64.getDecoder().decode(data.get(position).getBase64ImageString());
+        Bitmap decodedByteImg = BitmapFactory.decodeByteArray(decodedStringImg, 0, decodedStringImg.length);
+        Glide.with(holder.itemView.getContext())
+                .load(decodedByteImg)
+                .override(250, 250)
+                .circleCrop()
+                .into(holder.activityElementPhoto);
     }
 
     @Override
@@ -36,11 +53,21 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder> 
     }
 
     static class MyViewHolder extends RecyclerView.ViewHolder {
-        TextView activityName;
+        TextView activityElementName;
+        TextView activityElementLocation;
+        TextView activityStartTime;
+        TextView activityEndTime;
+        TextView activityId;
+        ImageView activityElementPhoto;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            activityName = itemView.findViewById(R.id.home_activity_name);
+            activityElementName = itemView.findViewById(R.id.home_activity_name);
+            activityElementPhoto = (ImageView) itemView.findViewById(R.id.home_activity_image);
+            activityElementLocation = itemView.findViewById(R.id.home_activity_location);
+            activityStartTime = itemView.findViewById(R.id.home_start);
+            activityEndTime = itemView.findViewById(R.id.home_end);
+            // activityId = itemView.findViewById(R.id.home_hidden_id);
         }
     }
 }
